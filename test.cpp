b494647ca2ac2d930cd98c878bf881bffefa72c0
unknown <tsaiyu0926.bt11@nycu.edu.tw>
2024-07-10 16:35:24 +0800

    first

---
+++ test.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+    /*
+    int a;
+    scanf("%d",&a);
+    printf("a is %d",a);
+    */
+    /*
+    int test = 1234567890;
+    double pi = 3.141592653589793;
+    printf("%20d\n",test);//to right
+    printf("%-20d stop\n",test);//to left
+    printf("%020d\n",test);//filled by zero
+    printf("%.2f\n",pi);//precision
+    printf("%20.5f\n",pi);
+    printf("%0.30f\n",pi);
+    */
+    /*
+    char str[30];
+    scanf("%s",&str);
+    printf("%s",str);
+    */
+    printf("你好");
+    return 0;
+}
